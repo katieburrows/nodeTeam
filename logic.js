a3ac49881f433f0d3b30d6cb0c1f1ad1197bf9a7
katieburrows <katie_burrows@me.com>
2019-06-08 18:50:26 -0600

    added prototype methods

--- logic.js
+++ logic.js
@@ -9,3 +9,27 @@ function Player(name, position, offense, defense) {
     this.offense = offense;
     this.defense = defense;
 }
+
+Player.prototype.goodGame = function() {
+    if(Math.floor(Math.random() * 2) === 0){
+        this.offense++;
+        console.log(`${this.name}'s offense has gone up\n------------`);
+    } else {
+        this.defense++;
+        console.log(`${this.name}'s defense has gone up!\n-----------`);
+    }
+};
+
+Player.prototype.badGame = function () {
+    if(Math.floor(Math.random() * 2) === 0){
+        this.offense--;
+        console.log(`${this.name}'s offense has gone down!\n----------`);
+    } else {
+        this.defense--;
+        console.log(`${this.name}'s defense has gone down!\n----------`)
+    }
+}
+
+Player.prototype.printStats = function () {
+    console.log(`Name: ${this.name}\nPosition: ${this.position}\nOffense: ${this.offense}\nDefense: ${this.defense}`);
+}
